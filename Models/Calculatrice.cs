15642509640e3964ee190c3c2ffb59923978b653
cbourdin <cbourdin@R1-22-009>
2022-04-04 09:29:29 +0200

    Ajoutez des fichiers projet.

---
+++ Models/Calculatrice.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculatrice.Models
+{
+    public struct Calculatrice
+    {
+        public double OperandeEnCours { get; set; }
+    }
+}
